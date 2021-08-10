98fe111781e34518c318d6e8e4040f07f305e72c
Duong <duongf5vcu@gmail.com>
2021-08-10 16:20:37 +0700

    add favicon.ico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>XYZ-home page</title>
-
+        <link rel="shortcut icon"  href="img/favicon.png">
+        <link rel="icon" type="image/gif" href="img/favicon.png">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
