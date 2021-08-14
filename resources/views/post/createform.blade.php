7b9976d5d4ae22abdf17055dc4d7119b220dfc0e
Duong <duongf5vcu@gmail.com>
2021-08-14 17:53:02 +0700

    add new post

--- resources/views/post/createform.blade.php
+++ resources/views/post/createform.blade.php
@@ -1,22 +1,41 @@
-<h1>Create Post</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+    <style>
+        #title{
+            width: 500px;
+        }
+        h1,form{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Create Post</h1>
+        <h1><a href="/">Home page</a></h1>
+    <form action="{{route('baiviet.store')}}" method = "POST">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name= "title" id="title">
+        </div>
+        <div>
+            <label for="title">description</label>
+            <textarea name="description" id="" cols="60" rows="10"></textarea>
+        </div>
+        <div>
+            <label for="title">Src</label>
+            <input type="text" name= "src">
+        </div>
+        <div>
+            <label for="title">cate</label>
+            <input type="text" name= "cate">
+        </div>
+        <button type = "submit">Post</button>
+    </form> 
+</body>
+</html>
 
-<form action="{{route('baiviet.store')}}" method = "POST">
-    @csrf
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name= "title">
-    </div>
-    <div>
-        <label for="title">description</label>
-        <textarea name="description" id="" cols="20" rows="10"></textarea>
-    </div>
-    <div>
-        <label for="title">Src</label>
-        <input type="text" name= "src">
-    </div>
-    <div>
-        <label for="title">cate</label>
-        <input type="text" name= "cate">
-    </div>
-    <button type = "submit">Post</button>
-</form> 
--- resources/views/post/editform.blade.php
+++ resources/views/post/editform.blade.php
@@ -1,24 +1,44 @@
-<h1>Edit form</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <style>
+        #title{
+            width: 500px;
+        }
+        h1,form{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Edit form</h1>
+    <form action="{{route('baiviet.update',$post->id)}}" method = "POST">
+        
+        @csrf
+        @method('PATCH')
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name= "title" id="title" value="{{ $post -> title }}">
+        </div>
+        <div>
+            <label for="description">description</label>
+            <textarea name="description" id="" cols="60" rows="10">{{$post -> description}}</textarea>
+        </div>
+        <div>
+            <label for="src">Src</label>
+            <input type="text" name= "src" value="{{$post -> src}}">
+        </div>
+        <div>
+            <label for="cate">Cate</label>
+            <input type="text" name= "cate" value="{{ $post ->cate}}">
+        </div>
+        <button type = "submit">Post</button>
+    </form>
+</body>
+</html>
 
-<form action="{{route('baiviet.update',$post->id)}}" method = "POST">
-    
-    @csrf
-    @method('PATCH')
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name= "title" value="{{ $post -> title }}">
-    </div>
-    <div>
-        <label for="description">description</label>
-        <textarea name="description" id="" cols="20" rows="10">{{$post -> description}}</textarea>
-    </div>
-    <div>
-        <label for="src">Src</label>
-        <input type="text" name= "src" value="{{$post -> src}}">
-    </div>
-    <div>
-        <label for="cate">Cate</label>
-        <input type="text" name= "cate" value="{{ $post ->cate}}">
-    </div>
-    <button type = "submit">Post</button>
-</form>
+
+
