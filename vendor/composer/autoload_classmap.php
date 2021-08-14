118f95d383b140dd19a89030de30c1109145421e
Duong <duongf5vcu@gmail.com>
2021-08-14 10:31:53 +0700

    dont make detail table

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -28,6 +28,7 @@ return array(
     'App\\Http\\Middleware\\TrimStrings' => $baseDir . '/app/Http/Middleware/TrimStrings.php',
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
+    'App\\Models\\Detail' => $baseDir . '/app/Models/Detail.php',
     'App\\Models\\Post' => $baseDir . '/app/Models/Post.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
     'App\\Providers\\AuthServiceProvider' => $baseDir . '/app/Providers/AuthServiceProvider.php',
