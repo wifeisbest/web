9849d499f7898d53fa8a0c7979646b950fbf4a5e
Duong <duongf5vcu@gmail.com>
2021-08-19 11:41:03 +0700

    add google tak

--- resources/views/layouts/detail.blade.php
+++ resources/views/layouts/detail.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="vi" >
 <head>
+        <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KFTPCDZ');</script>
+    <!-- End Google Tag Manager -->
     <title>{{$title}}</title>
     <link rel="shortcut icon"  href="img/favicon.png">
     <link rel="icon" type="image/gif" href="img/favicon.png">
@@ -42,6 +49,10 @@
     
 </head>
 <body>
+        <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFTPCDZ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('includes.header')        
         @yield('content')
     @include('includes.footer')
