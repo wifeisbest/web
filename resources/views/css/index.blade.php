0eb82219d99c3f13d92c714a71304c792064abfc
Duong <duongf5vcu@gmail.com>
2021-08-25 10:44:29 +0700

    edit .htaccess

--- resources/views/css/index.blade.php
+++ resources/views/css/index.blade.php
@@ -7,21 +7,21 @@
 
     @foreach($posts as $post)
     
-    <section>    
-        
-        <article class="section__article" itemscope itemtype="http://schema.org/Article">
-                <a itemprop="url" href="/{{$post -> slug}}" title="{{$post -> title}}" >
-                    <h1 class="section__title" itemprop="name"> {{$post -> title}} </h1>
-                    <div class="section__wrap" itemprop="description">                                                                                                                     
-                        <p class="section__description"> 
-                            <img itemprop="image" title="{{$post -> title}}" src="img/{{$post -> src}}.png" class="section__img" alt="{{$post -> title}}">{{$post -> description}}
-                        </p>                   
-                    </div>                                
-                </a>
-            </article>       
-    </section>  
+        <section>    
+            
+            <article class="section__article" itemscope itemtype="http://schema.org/Article">
+                    <a itemprop="url" href="/{{$post -> slug}}" title="{{$post -> title}}" >
+                        <h1 class="section__title" itemprop="name"> {{$post -> title}} </h1>
+                        <div class="section__wrap" itemprop="description">                                                                                                                     
+                            <p class="section__description"> 
+                                <img itemprop="image" title="{{$post -> title}}" src="img/{{$post -> src}}.png" class="section__img" alt="{{$post -> title}}">{{$post -> description}}
+                            </p>                   
+                        </div>                                
+                    </a>
+                </article>       
+        </section>  
     
-@endforeach
+    @endforeach
 </div>
 @endsection
 
