ffce2368f16350831411eed721dbfabb4d23337e
Duong <duongf5vcu@gmail.com>
2021-08-26 09:23:27 +0700

    add dmca

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -6,5 +6,11 @@
             <h4>Blog được thiết kế theo mẫu <a href="https://laptrinhcuocsong.com/">laptrinhcuocsong.com</a> </h4>
         </div>
         <h5>&#169; 2021 copyright all reserved</h5>
+        <div>
+            <a href="//www.dmca.com/Protection/Status.aspx?ID=77c48b51-ad3a-4044-ad11-d2e387f2242b" title="DMCA.com Protection Status" class="dmca-badge">
+            <img src ="https://images.dmca.com/Badges/dmca_protected_16_120.png?ID=77c48b51-ad3a-4044-ad11-d2e387f2242b"  alt="DMCA.com Protection Status" /></a>            
+            <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+        
+        </div>
 </footer>
 
