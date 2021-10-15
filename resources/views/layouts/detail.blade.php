0e3bd4ef4794479d4bfbe89670551d265e03452c
Duong Nguyen <duongf5vcu@gmail.com>
2021-10-15 14:15:27 +0700

    fixedend

--- resources/views/layouts/detail.blade.php
+++ resources/views/layouts/detail.blade.php
@@ -83,7 +83,7 @@
 
     <!-- <div id="fb-root"></div>
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v12.0&appId=4314618238628568&autoLogAppEvents=1" nonce="9PrrDQnX"></script> -->
-        yield('facebook')
+    @yield('facebook')
     <script src="js/main.js"></script>
     
 </body>
